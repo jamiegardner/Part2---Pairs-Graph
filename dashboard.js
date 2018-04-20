8de3bbc03f001e8e056f0fced04094bf0c4bdf87
Jamie Gardner <jagardner4@uclan.ac.uk>
2018-04-20 22:45:09 +0100

    Update: Colour changes

--- dashboard.js
+++ dashboard.js
@@ -9,10 +9,27 @@ function circle1() {
     document.getElementById("circle1").setAttribute('r', "20" );
 }
 
-function colour() {
+function black() {
     document.getElementById("circle").setAttribute('fill', "#000");
-    document.getElementById("circle1").setAttribute('fill');
+    document.getElementById("circle1").setAttribute('fill', "#000");
 }
 
+function orange() {
+    document.getElementById("circle").setAttribute('fill', "#FFA500");
+    document.getElementById("circle1").setAttribute('fill', "#FFA500");
+}
 
-  
+function purple() {
+    document.getElementById("circle").setAttribute('fill', "#551A8B");
+    document.getElementById("circle1").setAttribute('fill', "#551A8B");
+}
+
+function green() {
+    document.getElementById("circle").setAttribute('fill', "#00ff00 ");
+    document.getElementById("circle1").setAttribute('fill', "#00ff00 ");
+}
+
+function grey() {
+    document.getElementById("circle").setAttribute('fill', "#ccc");
+    document.getElementById("circle1").setAttribute('fill', "#ccc");
+}
