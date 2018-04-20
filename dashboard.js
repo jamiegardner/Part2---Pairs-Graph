34fb07571b0d3cfa447123e0d3fad92bf39dac56
Jamie Gardner <jagardner4@uclan.ac.uk>
2018-04-20 19:53:03 +0100

    Initial draft

---
+++ dashboard.js
@@ -0,0 +1,18 @@
+
+  function circle() {
+    document.getElementById("circle").setAttribute('r', "50" );
+    document.getElementById("circle1").setAttribute('r', "50" );
+}
+
+function circle1() {
+    document.getElementById("circle").setAttribute('r', "10" );
+    document.getElementById("circle1").setAttribute('r', "20" );
+}
+
+function colour() {
+    document.getElementById("circle").setAttribute('fill', "#000");
+    document.getElementById("circle1").setAttribute('fill');
+}
+
+
+  
